52842aead72644f6340598ff9391b5d2b91e97b4
PandaX185 <mouss3abbb@gmail.com>
2024-08-16 15:57:35 +0300

    init task manager

---
+++ lib/task_manager/tasks/task.ex
@@ -0,0 +1,21 @@
+defmodule TaskManager.Tasks.Task do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  schema "tasks" do
+    field :status, :string
+    field :description, :string
+    field :title, :string
+
+    timestamps(type: :utc_datetime)
+  end
+
+  @doc false
+  def changeset(task, attrs) do
+    task
+    |> cast(attrs, [:title, :description, :status])
+    |> validate_required([:title, :status])
+    |> validate_length(:title, min: 3)
+    |> validate_length(:description, max: 250)
+  end
+end
